9c6fbe6cc4e254f9f885b70ea34bbb31f4583696
toni-dev <62844491+Toni-d-e-v@users.noreply.github.com>
2023-02-11 13:17:44 +0100

    Add files via upload

--- electron.js
+++ electron.js
@@ -3,10 +3,36 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const path = require("path");
 const isDev = require("electron-is-dev");
-
+// nativeImage, Tray
+const { nativeImage, Tray, Menu } = require('electron')
 let mainWindow;
-
+// create tray
+let tray = null;
+function createTray () {
+    const icon = path.join(__dirname, 'logo.png')
+    const trayicon = nativeImage.createFromPath(icon)
+    tray = new Tray(trayicon.resize({ width: 16 }))
+    const contextMenu = Menu.buildFromTemplate([
+      {
+        label: 'Show App',
+        click: () => {
+          createWindow()
+        }
+      },
+      {
+        label: 'Quit',
+        click: () => {
+          app.quit() // actually quit the app.
+        }
+      },
+    ])
+  
+    tray.setContextMenu(contextMenu)
+  }
 function createWindow() {
+    if (!tray) {
+        createTray()
+      }
 mainWindow = new BrowserWindow({ width: 900, height: 680, icon: path.join(__dirname, 'logo.png') });
     // disble menu bar and dev tools and maximize window
     mainWindow.setMenuBarVisibility(false);
@@ -21,13 +47,38 @@ mainWindow = new BrowserWindow({ width: 900, height: 680, icon: path.join(__dirn
 app.on("ready", createWindow);
 
 app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
+    // alert the user that app is still running
+    
+
 });
 
 app.on("activate", () => {
     if (mainWindow === null) {
         createWindow();
     }
+});
+
+// run the node in the background
+// on first run
+app.on('ready', () => {
+    // win documents/aves-data 
+    // mac ~/Library/Application Support/aves-data
+    // linux ~/.aves-data
+    // https://github.com/Aves-Project/aves-go/releases
+    //
+    let lastestVersion = '';
+    // fetch the latest version
+    fetch('https://api.github.com/repos/Aves-Project/aves-go/releases/latest')
+    .then(response => response.json())
+    .then(data => {
+        if (data.tag_name) {
+            lastestVersion = data.tag_name;
+        }
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    }
+    );
+    // check if the version is the same
+
 });
